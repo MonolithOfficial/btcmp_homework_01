40b03bc3a4ea12ebd9504e9962b24344ec8fcc36
DarkroomLaboratory <cotneaburjania@gmail.com>
2020-12-01 23:07:47 +0400

    initial commit

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,21 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+    @Test
+    public void executeTest(){
+        WebDriverManager.chromedriver().setup(); // System.setProperty()-ის დინამიური ალტერნატივა
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://the-internet.herokuapp.com");
+        driver.findElement(By.linkText("Forgot Password")).click();
+
+        driver.findElement(By.id("email")).sendKeys("cotneaburjania@gmail.com");
+        driver.findElement(By.id("form_submit")).click();
+    }
+
+}
